26be9cb1df47ac44c7cd812b5cfb3830a8c47c24
Claudio Carnino <claudio@tugulab.org>
2025-01-13 17:03:41 +0000

    feat: fetching the quote manually

---
+++ next-example/src/app/hooks/useSwapTx.ts
@@ -0,0 +1,49 @@
+import useSWR from "swr";
+
+const fetcher = async ([quote, userPublicKey]: [
+  quote: unknown,
+  userPublicKey: string
+]) => {
+  const response = await fetch("https://quote-api.jup.ag/v6/swap", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      // quoteResponse from /quote api
+      quoteResponse: quote,
+      // user public key to be used for the swap
+      userPublicKey, //: wallet.publicKey.toString(),
+      // auto wrap and unwrap SOL. default is true
+      wrapAndUnwrapSol: true,
+      // feeAccount is optional. Use if you want to charge a fee.  feeBps must have been passed in /quote API.
+      // feeAccount: "fee_account_public_key"
+      // custom priority fee
+      // prioritizationFeeLamports: "auto", // in normal conditions, use auto
+      prioritizationFeeLamports: {
+        autoMultiplier: 2, // when there's a lot of congestions for the pair, double the suggested fee
+      },
+    }),
+  });
+  const { swapTransaction } = await response.json();
+  return swapTransaction;
+};
+
+type UseQuoteProps = {
+  quote: unknown | undefined;
+  userPublicKey: string | undefined;
+  isEnabled: boolean;
+};
+
+export default function useQuote({
+  quote,
+  userPublicKey,
+  isEnabled,
+}: UseQuoteProps) {
+  const arePropsValid = quote && userPublicKey;
+
+  return useSWR(
+    isEnabled && arePropsValid ? [quote, userPublicKey] : undefined,
+    fetcher
+  );
+}
--- next-example/src/app/page.tsx
+++ next-example/src/app/page.tsx
@@ -4,6 +4,7 @@ import { usePrivy } from "@privy-io/react-auth";
 import ConnectWalletButton from "./components/ConnectWalletButton";
 import DisconnectWalletButton from "./components/DisconnectWalletButton";
 import useQuote from "./hooks/useQuote";
+import { useState } from "react";
 
 const SELL_TOKEN_ADDRESS = "So11111111111111111111111111111111111111112"; // Sol
 const BUY_TOKEN_ADDRESS = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"; // USDC
@@ -12,6 +13,9 @@ const SLIPPAGE_BPS = 50;
 
 export default function Home() {
   const { ready, authenticated } = usePrivy();
+
+  const [shouldFetchQuote, setShouldFetchQuote] = useState(false);
+
   const isConnected = ready && authenticated;
 
   const { data: quoteData, isLoading: isLoadingQuote } = useQuote({
@@ -19,11 +23,11 @@ export default function Home() {
     buyTokenAddress: BUY_TOKEN_ADDRESS,
     sellAmount: SELL_AMOUNT,
     slippageBps: SLIPPAGE_BPS,
-    isEnabled: isConnected,
+    isEnabled: isConnected && shouldFetchQuote,
   });
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="p-8">
       <main className="flex flex-col gap-36 row-start-2 items-center sm:items-start">
         <h1 className="text-3xl font-bold">Next/React app example</h1>
 
@@ -45,6 +49,34 @@ export default function Home() {
           <section className="flex flex-col gap-4">
             <h2 className="text-2xl font-bold">2. Fetch quote</h2>
             <div className="flex flex-col gap-2">
+              {!shouldFetchQuote && (
+                <button
+                  onClick={() => setShouldFetchQuote(true)}
+                  className="bg-blue-500 text-white px-4 py-2 rounded-md"
+                >
+                  Fetch quote
+                </button>
+              )}
+
+              {isLoadingQuote && "Loading quote..."}
+
+              {quoteData && (
+                <div>
+                  <div>Quote:</div>
+                  <pre className="font-mono text-xs bg-gray-100 p-4 rounded-md h-48 overflow-auto">
+                    {JSON.stringify(quoteData, null, 2)}
+                  </pre>
+                </div>
+              )}
+            </div>
+          </section>
+        )}
+
+        {/* --- Fetch swap step --- */}
+        {/* {isConnected && quoteData && (
+          <section className="flex flex-col gap-4">
+            <h2 className="text-2xl font-bold">3. Fetch swap</h2>
+            <div className="flex flex-col gap-2">
               {isLoadingQuote ? (
                 "Loading quote..."
               ) : quoteData ? (
@@ -59,7 +91,7 @@ export default function Home() {
               )}
             </div>
           </section>
-        )}
+        )} */}
       </main>
     </div>
   );
